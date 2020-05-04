dfc758d38ca56a7dc81da937309e7e8870781167
NotYourSyn <60495598+NotYourSyn@users.noreply.github.com>
2020-05-03 22:12:59 -0400

    going over som of my answers making changes

--- index.js
+++ index.js
@@ -226,8 +226,9 @@ console.log(artists[8].name);
  * For example, if getArtistByIndex is invoked with the inventory and the number 0,
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
-function getArtistByIndex(artists,id) {
-    return `The artist at ${id} is ${artists.name}.`;
+function getArtistByIndex(artists,index) {
+    const artist = artists[index];
+    return `The artist at ${artist.id} is ${artist.name}.`;
   }
   
   /**
@@ -271,9 +272,8 @@ genre: Web Design,
 nationality: Your Nationality Here
 bio: Add 1-2 sentences (or use lorem ipsum) "*/
 
-function addArtist(info){
-
-    /* Code here */
+function addArtist(id,name,years,genre,nationality,bio){
+    let newArt = []
 
   }
 
